9917a18cddee87e8022488e4ee2423ff8a3ab666
Endeavour101 <162818011+Endeavour101@users.noreply.github.com>
2026-09-09 22:00:15 +0200

    exercises with strings  characters formatting

--- 01-freecodecamp/variables/03-working-with-strings.js
+++ 01-freecodecamp/variables/03-working-with-strings.js
@@ -60,7 +60,44 @@ console.log("x=x=x=x=x=x=x");
 const btn = document.getElementById('prompt-btn');
 const display = document.getElementById('display-prompt');
 btn.addEventListener('click', () => {
-  const customerName = prompt("What\'s your name?", "Moris");
+  const customerName = prompt("What's your name?", "Moris");
   display.textContent = `Hello dear ${customerName}!`;
 });
 
+let car = "!Ford Mustang";
+console.log(car.charCodeAt("0"));
+console.log(car.charCodeAt("1"));
+console.log(car.charAt("0"));
+console.log(car.charAt("1"));
+
+let char = String.fromCharCode(65);
+console.log(char);
+let char1 = String.fromCharCode(66);
+console.log(char1);
+
+alert( `My\n`.length ); // 3
+
+let str = `Bye`;
+
+// the first character
+alert( str[0] ); // H
+alert( str.at(0) ); // H
+
+// the last character
+alert( str[str.length - 1] ); // o
+/* alert( str.at(-1) );
+alert( str.at(-2) );
+alert( str.at(-3) ); */
+
+for (let char of "Bye") {
+  alert(char); // H,e,l,l,o (char becomes "H", then "e", then "l" etc)
+}
+
+let str1 = 'Hi';
+str1 = 'h' + str1[1]; // replace the string
+alert( str1 ); // hi
+
+alert( 'Piazza'.toUpperCase() ); // INTERFACE
+alert( 'Piazza'.toLowerCase() ); // interface
+
+alert( 'Piazza'[0].toLowerCase() ); // 'i'
